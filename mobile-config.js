93466f24f3f3dd8fba8c212a33b09f4c1512d3f7
Gabriel Vermeulen <gatux94@gmail.com>
2015-10-25 18:41:06 +0100

    Android support

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+	id: 'randomID',
+	name: 'eHealth',
+	description: 'eHealth',
+	author: 'ENSEIRB-MATMECA Telecom S9 Group 4 2015',
+	email: 'gatux94@gmail.com',
+	website: 'http://ehealth.gatux.fr'
+});
